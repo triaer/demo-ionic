2484f034bb39fcf014bfa3ec77411921d996cbb5
tri.mai <tri.mai@LGVN181086>
2020-10-06 17:20:56 +0700

    delete photo additional file

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { Photo, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, 
+              public actionSheetController: ActionSheetController) {}
+  
 
   addPhotoToGallery() {
   this.photoService.addNewToGallery();
@@ -15,5 +18,27 @@ export class Tab2Page {
 
   async ngOnInit() {
     await this.photoService.loadSaved();
+  }
+  
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 }
