614a8f5affeb91309ec56cc9a6a5a563d82fb911
tri.mai <tri.mai@LGVN181086>
2020-10-06 17:19:24 +0700

    add delete photo, fix duplicate save photo

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
-  CameraPhoto, CameraSource } from '@capacitor/core';
+  CameraPhoto, CameraSource, CameraDirection } from '@capacitor/core';
 
 import { Platform } from '@ionic/angular';
 
@@ -81,17 +81,19 @@ export class PhotoService {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri, 
       source: CameraSource.Camera, 
-      quality: 100 
+      quality: 100,
+      direction: CameraDirection.Front
     });
 
+    
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+    // this.photos.unshift({
+    //   filepath: "soon...",
+    //   webviewPath: capturedPhoto.webPath
+    // });
 
     Storage.set({
       key: this.PHOTO_STORAGE,
@@ -104,20 +106,41 @@ export class PhotoService {
     const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photoList.value) || [];
   
-    // more to come...
-    // Display the photo by reading into base64 format
+    // Easiest way to detect when running on the web:
+    // “when the platform is NOT hybrid, do this”
     if (!this.platform.is('hybrid')) {
+      // Display the photo by reading into base64 format
       for (let photo of this.photos) {
         // Read each saved photo's data from the Filesystem
         const readFile = await Filesystem.readFile({
             path: photo.filepath,
             directory: FilesystemDirectory.Data
         });
-
+  
         // Web platform only: Load the photo as base64 data
         photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
       }
     }
+  }
+
+  public async deletePicture(photo: Photo, position: number) {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
+  
+    // Update photos array cache by overwriting the existing photo array
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  
+    // delete photo file from filesystem
+    const filename = photo.filepath
+                        .substr(photo.filepath.lastIndexOf('/') + 1);
+  
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: FilesystemDirectory.Data
+    });
   }
 }
 
